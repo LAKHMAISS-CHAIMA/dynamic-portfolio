5434b0e626797761516b0943f71a32ae4c1c7233
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2025-01-31 17:39:12 +0100

    first push

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo.png" />
+        <h1>My portfolio</h1>
+        <button font-semibold text-whi>à propot</button>
+        <button>Mes projets</button>
+        <button>Contactez- moi</button>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
